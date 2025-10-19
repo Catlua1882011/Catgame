2d537953807be16ab2d787085bc960acb0bcc000
Catlua1882011 <138785252+Catlua1882011@users.noreply.github.com>
2025-10-19 23:05:05 +0700

    Add files via upload
    Signed-off-by: Catlua1882011 <138785252+Catlua1882011@users.noreply.github.com>

--- Crystal Hub Fruit.lua
+++ Crystal Hub Fruit.lua
@@ -5,11 +5,20 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TeleportService = game:GetService("TeleportService")
 local HttpService = game:GetService("HttpService")
 local CoreGui = game:GetService("CoreGui")
-local playerGui = player:WaitForChild("PlayerGui")
+
+if game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)") then
+    if game.Players.LocalPlayer.PlayerGui["Main (minimal)"]:FindFirstChild("ChooseTeam") then
+        repeat wait()
+            if game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)").ChooseTeam.Visible then
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("SetTeam","Pirates")
+            end
+        until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
+    end
+end
+Wait(0.8)
 local cg = game:GetService("CoreGui")
 local player = game:GetService("Players").LocalPlayer
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().Team = "Pirates"
 getgenv().Setting = {
     ["Delay Hop"] = 1,
     ["Webhook"] = {
@@ -30,32 +39,6 @@ getgenv().Setting = {
         ["Factory"] = true,
     }
 }
-if not player.Team then
-    if getgenv().Team == "Marines" then
-        ReplicatedStorage.Remotes.CommF_:InvokeServer("SetTeam", "Marines")
-    elseif getgenv().Team == "Pirates" then
-        ReplicatedStorage.Remotes.CommF_:InvokeServer("SetTeam", "Pirates")
-    end
-    repeat
-        task.wait(1)
-        local chooseTeam = playerGui:FindFirstChild("ChooseTeam", true)
-        local uiController = playerGui:FindFirstChild("UIController", true)
-        if chooseTeam and chooseTeam.Visible and uiController then
-            for _, v in pairs(getgc(true)) do
-                if type(v) == "function" and getfenv(v).script == uiController then
-                    local constant = getconstants(v)
-                    pcall(function()
-                        if (constant[1] == "Pirates" or constant[1] == "Marines") and #constant == 1 then
-                            if constant[1] == getgenv().Team then
-                                v(getgenv().Team)
-                            end
-                        end
-                    end)
-                end
-            end
-        end
-    until player.Team
-end
 
 if cg:FindFirstChild('Crystal hub') then
     cg:FindFirstChild('Crystal hub'):Destroy()
